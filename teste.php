56488b689e2e71663f18bbf168b3674cd1c712c9
danilorodri123 <44899618+danilorodri123@users.noreply.github.com>
2019-05-09 20:47:46 -0300

    teste

---
+++ teste.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Teste Variavel</title>
+</head>
+<body>
+	<?php
+		$nome = "Danilo";
+		$idade = 17;
+		echo "$nome tem $idade anos!";
+	?>
+</body>
+</html>
