1a0da75aece25067c2129b4a9af5f15284a519e4
danilorodri123 <44899618+danilorodri123@users.noreply.github.com>
2019-05-15 14:22:22 -0300

    Add files via upload

---
+++ Operadores aritméticos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OPERADORES</title>
+</head>
+<body>
+	<?php  
+		$n1 = 6;
+		$n2 = 10;
+		echo "O valor da soma é " . ($n1 + $n2);
+		echo "</br>O valor da multiplicação é " . ($n1 * $n2);
+		echo "</br>O valor da divisão é " . ($n1 / $n2);
+		echo "</br>O valor da subtração é " . ($n1 - $n2);
+	?>
+
+</body>
+</html>
